0b69e8fe06be5ee65e4818b691c5cf59895ef747
Wes Morgan <cap10morgan@gmail.com>
2014-05-12 10:13:19 -0600

    use released 1.0.0 version of squishy SQS library

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [org.clojure/tools.logging "0.2.6"]
                  [org.slf4j/slf4j-simple "1.7.7"]
                  [turbovote.resource-config "0.1.2-SNAPSHOT"]
-                 [democracyworks.squishy "1.0.0-SNAPSHOT"]
+                 [democracyworks.squishy "1.0.0"]
                  [clj-http "0.9.1"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}}
   :main ^{:skip-aot true} hermes-processor.core)
