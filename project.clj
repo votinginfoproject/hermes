5127ff166d5924b9011ae247c7e2f5c120088834
Wes Morgan <cap10morgan@gmail.com>
2014-05-12 10:12:55 -0600

    [WM] update deps to latest versions

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-simple "1.7.6"]
+                 [org.slf4j/slf4j-simple "1.7.7"]
                  [turbovote.resource-config "0.1.2-SNAPSHOT"]
                  [democracyworks.squishy "1.0.0-SNAPSHOT"]
                  [clj-http "0.9.1"]]
