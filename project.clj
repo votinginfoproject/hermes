53b6a83d75ac63cdf165b2ecadde41ddfac36810
Chris Shea <cmshea@gmail.com>
2014-05-19 12:35:25 -0400

    [CS WR] use resource-config 0.1.1

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
                  [org.slf4j/slf4j-simple "1.7.7"]
-                 [turbovote.resource-config "0.1.2-SNAPSHOT"]
+                 [turbovote.resource-config "0.1.1"]
                  [democracyworks.squishy "1.0.0"]
                  [clj-http "0.9.1"]]
   :profiles {:dev {:resource-paths ["dev-resources"]}
